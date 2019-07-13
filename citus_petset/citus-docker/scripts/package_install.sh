03cf57b5d4da3ff1e39b639c25b7646edc533d67
Joshua Kramer <joskra42@gmail.com>
2019-07-12 20:37:00 -0400

    Initial commit of modifications from pull

--- citus_petset/citus-docker/scripts/package_install.sh
+++ citus_petset/citus-docker/scripts/package_install.sh
@@ -1,48 +1,55 @@
 #!/bin/bash
 
-# setup dnf to pull PostgreSQL from yum.postgresql.org
-#dnf -y -q install https://download.postgresql.org/pub/repos/yum/9.6/fedora/fedora-23-x86_64/pgdg-fedora96-9.6-3.noarch.rpm
-rpm -i https://download.postgresql.org/pub/repos/yum/9.6/fedora/fedora-24-x86_64/pgdg-fedora96-9.6-3.noarch.rpm
+PGVER=11
+
+trap 'echo "# $BASH_COMMAND"' DEBUG
+
+# setup yum to pull PostgreSQL from yum.postgresql.org
+
+yum -y -q install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
 # install citus repo for up-to-date citus
-cp /scripts/config_file.repo /etc/yum.repos.d/citus-community.repo
+#cp /scripts/config_file.repo /etc/yum.repos.d/citus-community.repo
 
 # install some basics
-dnf -y -q install readline-devel
-dnf -y -q install hostname
+yum -y -q install readline-devel
+yum -y -q install hostname
+yum -y -q install epel-release
 
 # install postgresql and a bunch of accessories
-dnf -y -q install postgresql96
-dnf -y -q install postgresql96-server
-dnf -y -q install postgresql96-contrib
-dnf -y -q install postgresql96-devel postgresql96-libs
-dnf -y -q install python3-psycopg2
-dnf -y -q install citus_96
+yum -y -q install postgresql11
+yum -y -q install postgresql11-server
+yum -y -q install postgresql11-contrib
+yum -y -q install postgresql11-devel postgresql11-libs
+yum -y -q install python36-psycopg2
+yum -y -q install citus_11
 
 # set up SSL certs
-dnf -y -q install openssl openssl-devel
+yum -y -q install openssl openssl-devel
 sh /etc/ssl/certs/make-dummy-cert /etc/ssl/certs/postgres.cert
 chown postgres:postgres /etc/ssl/certs/postgres.cert
 
 # put binaries in postgres' path
-ln -s /usr/pgsql-9.6/bin/pg_ctl /usr/bin/
-ln -s /usr/pgsql-9.6/bin/pg_config /usr/bin/
-ln -s /usr/pgsql-9.6/bin/pg_controldata /usr/bin/
-ln -s /usr/pgsql-9.6/bin/initdb /usr/bin/
-ln -s /usr/pgsql-9.6/bin/postgres /usr/bin/
+ln -s /usr/pgsql-11/bin/pg_ctl /usr/bin/
+ln -s /usr/pgsql-11/bin/pg_config /usr/bin/
+ln -s /usr/pgsql-11/bin/pg_controldata /usr/bin/
+ln -s /usr/pgsql-11/bin/initdb /usr/bin/
+ln -s /usr/pgsql-11/bin/postgres /usr/bin/
 
 #  install extensions
-#dnf -y -q install postgresql-${PGVER}-postgis-2.1 postgresql-${PGVER}-postgis-2.1-scripts
+#yum -y -q install postgresql-${PGVER}-postgis-2.1 postgresql-${PGVER}-postgis-2.1-scripts
 
 # install python requirements
-dnf -y -q install python3-pip
-dnf -y -q install python3-devel
+yum -y -q install python36-pip
+yum -y -q install python36-devel
 pip3 install -U requests
 
 # install WAL-E
 # pip install -U six
 # pip install -U wal-e
-# dnf -y -q install daemontools
-# dnf -y -q install lzop pv
+# yum -y -q install daemontools
+# yum -y -q install lzop pv
 
-# clean up dnf cache to shrink image
-dnf clean all
+# clean up yum cache to shrink image
+yum clean all
+
